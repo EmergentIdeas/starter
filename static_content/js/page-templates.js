aafa3693b3b167264515c5269a8538ec4f8aefa9
Dan Kolz <dpkolz@gmail.com>
2016-04-03 22:35:04 -0500

    new packaging and node

--- static_content/js/page-templates.js
+++
@@ -1,8 +0,0 @@
-$.get('/templates/page.templates', function(data) {
-	Tripartite.parseTemplateScript(data);
-	if(Tripartite.onTemplatesLoaded) {
-		Tripartite.onTemplatesLoaded();
-		Tripartite.onTemplatesLoaded = null;
-	}
-	
-});
